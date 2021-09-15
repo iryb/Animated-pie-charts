56b9f4bcc8eca3ad83cc87894956d7ea0214115e
Iryna <iryna.burianovata@gmail.com>
2021-09-15 14:29:10 +0300

    Settings page link

--- animated-pie-charts.php
+++ animated-pie-charts.php
@@ -30,16 +30,37 @@ defined('ABSPATH') or die('You can\t access this file.');
 if( !class_exists('AnimatedPieCharts') ) {
     class AnimatedPieCharts
     {
-//        function __construct() {
-//            $this->create_post_type();
-//        }
 
-        static function register_admin() {
-            add_action( 'wp_enqueue_scripts', array( 'AnimatedPieCharts', 'enqueue_admin' ) );
+        public $plugin;
+
+        function __construct() {
+            $this->plugin = plugin_basename( __FILE__ );
         }
 
-        static function register() {
-            add_action( 'wp_enqueue_scripts', array( 'AnimatedPieCharts', 'enqueue' ) );
+        function register_admin() {
+            add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_admin' ) );
+
+            add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+
+            add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
+        }
+
+        function settings_link( $links ) {
+            $settings_link = '<a href="admin.php?page=apc_charts_plugin">Settings</a>';
+            array_push( $links, $settings_link );
+            return $links;
+        }
+
+        function add_admin_pages() {
+            add_menu_page( 'Animated Pie Charts', 'Charts', 'manage_options', 'apc_charts_plugin', array($this, 'admin_index'), 'dashicons-store', 110 );
+        }
+
+        function admin_index() {
+            require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
+        }
+
+        function register() {
+            add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
         }
 
         function activate() {

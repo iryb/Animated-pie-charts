83196dadf74a823f29c52380eb6db3b86a8ddba8
Iryna <iryna.burianovata@gmail.com>
2021-09-19 11:32:21 +0300

    Settings link class

--- inc/Base/SettingsLinks.php
+++ inc/Base/SettingsLinks.php
@@ -1,1 +1,14 @@
 <?php
+namespace Inc\Base;
+
+class SettingsLinks {
+    public function  register() {
+        add_filter( "plugin_action_links_" . PLUGIN, array( $this, 'settings_link' ) );
+    }
+
+    public function settings_link( $links ) {
+        $settings_link = '<a href="admin.php?page=apc_charts_plugin">Settings</a>';
+        array_push( $links, $settings_link );
+        return $links;
+    }
+}

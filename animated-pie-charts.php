88d032ba54613ac23b817eeaa0dc878ef7de065e
Iryna <iryna.burianovata@gmail.com>
2021-09-19 11:28:40 +0300

    Added activation/deactivation hooks

--- animated-pie-charts.php
+++ animated-pie-charts.php
@@ -27,13 +27,39 @@ along with Animated Pie Charts. If not, see https://www.gnu.org/licenses/gpl-2.0
 
 defined('ABSPATH') or die('You can\t access this file.');
 
+// Require once the Composer Autoload
 if(file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' )) {
     require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
+// Define constants
 define ( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define ( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define ( 'PLUGIN', plugin_basename( __FILE__ ) );
 
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+/*
+ * The code that runs during plugin activation
+ */
+function activate_apc_charts_plugin() {
+    Activate::activate();
+}
+
+/*
+ * The code that runs during plugin deactivation
+ */
+function deactivate_apc_charts_plugin() {
+    Deactivate::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_apc_charts_plugin');
+register_deactivation_hook( __FILE__, 'deactivate_apc_charts_plugin');
+
+/*
+ * Initialize all the core classes of the plugin
+ */
 if( class_exists('Inc\\Init') ) {
     Inc\Init::register_services();
 }

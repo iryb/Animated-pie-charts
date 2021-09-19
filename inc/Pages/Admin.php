836f7ec4010dfbf3fd0ee4fde5cc737a4b3af2ce
Iryna <iryna.burianovata@gmail.com>
2021-09-19 12:39:31 +0300

    Created Settings Api class

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -3,18 +3,40 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController {
-    public function register() {
-        add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+    public $settings;
+
+    public function __construct()
+    {
+        $this->settings = new SettingsApi();
     }
 
-    function add_admin_pages() {
-        add_menu_page( 'Animated Pie Charts', 'Charts', 'manage_options', 'apc_charts_plugin', array($this, 'admin_index'), 'dashicons-store', 110 );
+    public function register()
+    {
+        $pages = [
+            [
+                'page_title' => 'Animated Pie Charts',
+                'menu_title' => 'Charts',
+                'capability' => 'manage_options',
+                'menu_slug'  => 'apc_charts_plugin',
+                'callback'   => function() {echo '<h1>Plugin</h1>'; },
+                'icon_url'   => 'dashicons-store',
+                'position'   => 110
+            ]
+        ];
+        $this->settings->addPages( $pages )->register();
     }
 
-    function admin_index() {
-        require_once $this->plugin_path . 'templates/admin.php';
-    }
+//    function add_admin_pages()
+//    {
+//        add_menu_page( 'Animated Pie Charts', 'Charts', 'manage_options', 'apc_charts_plugin', array($this, 'admin_index'), 'dashicons-store', 110 );
+//    }
+//
+//    function admin_index()
+//    {
+//        require_once $this->plugin_path . 'templates/admin.php';
+//    }
 
 }
